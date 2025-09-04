89d3bc26ad3ec8b3efea7c6960586d070827310e
Caleb Greene <calebgreene@macbookair.mynetworksettings.com>
2025-09-04 15:43:45 -0400

    update

--- main.py
+++ main.py
@@ -4,6 +4,11 @@ class main(customtkinter.CTk):
     def __init__(self):
         super().__init__()
 
-        self.geometry("750x250")
+        self.geometry("800x400")
         self.title("RSAnalysis")
         self.resizable(False, False)
+
+
+if __name__ == "__main__":
+    app = main()
+    app.mainloop()
