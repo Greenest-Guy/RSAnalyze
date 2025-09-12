c32d56a3aa5e52e8af76b82de760b55701582f24
Caleb Greene <greenestcaleb@gmail.com>
2025-09-11 23:08:51 -0400

    added BG

--- logic.py
+++ logic.py
@@ -1,6 +1,7 @@
 from sympy import isprime
 import math
 
+
 class Logic:
     def __init__(self, p, q):
         if not (isprime(p) and isprime(q)):
@@ -15,8 +16,9 @@ class Logic:
         self.phi_n = (p - 1) * (q - 1)
 
         # Calculate e
-        self.e = FERMAT if (self.phi_n > FERMAT and self.iscoprime(self.phi_n, FERMAT)) else 3
-        
+        self.e = FERMAT if (self.phi_n > FERMAT and self.iscoprime(
+            self.phi_n, FERMAT)) else 3
+
         if self.e != FERMAT:
             while not self.iscoprime(self.e, self.phi_n):
                 self.e += 2
@@ -24,39 +26,30 @@ class Logic:
         # Calculate d
         self.d = pow(self.e, -1, self.phi_n)
 
-
     @staticmethod
-    def iscoprime(a, b): 
+    def iscoprime(a, b):
         return math.gcd(a, b) == 1
-
 
     def get_p(self):
         return self.p
 
-
     def get_q(self):
         return self.q
 
-
     def get_n(self):
         return self.n
-    
 
     def get_e(self):
         return self.e
 
-
     def get_d(self):
         return self.d
-    
 
     def get_eulers_totient(self):
         return self.phi_n
-    
 
     def get_public_key(self):
         return (self.n, self.e)
-    
 
     def get_private_key(self):
         return (self.n, self.d)
--- main.py
+++ main.py
@@ -1,15 +1,57 @@
-from RSAnalysis.pollards_rho.pollards_rho import PollardsRho
-from logic import logic
-import customtkinter
+from pollards_rho.pollards_rho import PollardsRho
+from CTkToolTip import CTkToolTip
+from customtkinter import *
+from logic import Logic
+from PIL import Image
 
 
-class main(customtkinter.CTk):
+class main(CTk):
     def __init__(self):
         super().__init__()
-
+        set_appearance_mode("dark")
         self.geometry("800x400")
-        self.title("RSAnalysis")
+        self.title("RSAnalyze")
         self.resizable(False, False)
+
+        dir_path = os.path.dirname(os.path.abspath(__file__))
+
+        background_image = CTkImage(light_image=Image.open(os.path.join(dir_path, "BG.png")),
+                                    dark_image=Image.open(
+                                        os.path.join(dir_path, "BG.png")),
+                                    size=(800, 400))
+        CTkLabel(self, image=background_image, text="").place(x=0, y=0)
+
+        # colors
+        self.purple = "#7b00ff"
+        self.dark_purple = "#6100c9"
+        self.light_grey = "#2f2f2f"
+        self.dark_grey = "#222222"
+        self.white = "#ffffff"
+        self.black = "#000000"
+
+        # ENTRY p
+        p_entry = CTkEntry(
+            self, placeholder_text="Prime 1 (p)", bg_color=self.light_grey, width=150, height=30, font=("Inter", 12))
+        p_entry.place(x=25, y=75)
+
+        # ENTRY q
+        q_entry = CTkEntry(
+            self, placeholder_text="Prime 2 (q)", bg_color=self.light_grey, width=150, height=30, font=("Inter", 12))
+        q_entry.place(x=25, y=125)
+
+        # BUTTON auto-generate primes
+        auto_primes_button = CTkButton(self, width=150, height=25, bg_color=self.light_grey, text="Randomize",
+                                       fg_color=self.purple, hover_color=self.dark_purple, text_color=self.white, font=("Inter", 16))
+
+        auto_primes_button.place(x=25, y=175)
+
+        # BUTTON Factorize
+        Factorize_button = CTkButton(self, width=150, height=30, bg_color=self.light_grey, text="Generate",
+                                     fg_color=self.purple, hover_color=self.dark_purple, text_color=self.white, font=("Inter", 16))
+
+        Factorize_button.place(x=25, y=350)
+
+        # TEXTBOX
 
 
 if __name__ == "__main__":
